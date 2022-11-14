7d3dc6a032bbd214119d37a56f9426455074f4de
Xin Wang <xx.wang@student.avans.nl>
2022-11-14 19:42:45 +0100

    Added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './features/about-me/about-me.component';
 import { HomeComponent } from './features/home/home.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component'
 import { FooterComponent } from './shared/footer/footer.component';
-import { NormalComicsListComponent } from './features/normal-comics-list/normal-comics-list.component';
-import { OwnListComponent } from './features/own-list/own-list.component';
-import { RecommendedlistComponent } from './features/recommendedlist/recommendedlist.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DetailComicComponent } from './features/detail-comic/detail-comic.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import {MaterialExampleModule} from './material.module';
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
     HomeComponent,
-    NavbarComponent,
+    NavBarComponent,
     FooterComponent,
-    NormalComicsListComponent,
-    OwnListComponent,
-    RecommendedlistComponent,
     LoginComponent,
-    DetailComicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
