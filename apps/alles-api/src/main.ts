f3d07b3ec200fb45a9942f39d29e5c15ceb24b1c
Xin Wang <xx.wang@student.avans.nl>
2022-11-22 15:03:48 +0100

    Env variables work

--- apps/alles-api/src/main.ts
+++ apps/alles-api/src/main.ts
@@ -2,7 +2,7 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
+import 'dotenv';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
