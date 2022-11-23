9915138e997051b8ea1da8a2be2ba9012e945baf
xin <xin20wang@outlook.com>
2022-11-23 10:16:33 +0100

    Added comment

--- apps/alles-api/src/app/Controllers/Data.module.ts
+++ apps/alles-api/src/app/Controllers/Data.module.ts
@@ -6,6 +6,7 @@ import { AuthController } from "./Auth/Auth.controller";
 import { UserController } from "./Users/Userr.controller";
 
 @Module({
+    //Legt database connectie met de cloud database van mongodb.
     imports: [MongooseModule.forRoot(`mongodb+srv://User33:Das4reich@beifang.mwlxo7r.mongodb.net/?retryWrites=true&w=majority`)],
     controllers: [AppController, UserController, AuthController],
     providers: [AppService],
