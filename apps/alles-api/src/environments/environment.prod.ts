32aefc65314452c0473196a35e2c28713dc5ae27
Xin Wang <xx.wang@student.avans.nl>
2022-11-25 14:18:15 +0100

    Fixed .env variabels

--- apps/alles-api/src/environments/environment.prod.ts
+++ apps/alles-api/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  Database:`mongodb+srv://User33:${process.env.MONGO_CODE}@beifang.mwlxo7r.mongodb.net/?retryWrites=true&w=majority`
+  Database:`mongodb+srv://User33:${process.env["MONGO_CODE"]}@beifang.mwlxo7r.mongodb.net/?retryWrites=true&w=majority`
 };
