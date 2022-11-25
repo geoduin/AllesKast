f01898fb3ffc5c379befbe31aab98106c9758230
Xin Wang <38434237+geoduin@users.noreply.github.com>
2022-11-25 13:45:18 +0100

    Is going to be replayed with a respelled service

--- libs/Services/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/HttpClients/Story.client';
-export * from './lib/HttpClients/User.client';
-export * from './lib/ConfigModules/WebHttp.service';
-export * from './lib/ConfigModules/WebHttp.module';
