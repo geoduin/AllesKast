ddd2229a625f74aac8d826d0ed9eec713626880d
Xin Wang <xx.wang@student.avans.nl>
2022-11-27 14:59:27 +0100

    Second push

--- libs/services/src/lib/HttpClients/Story.client.ts
+++ libs/services/src/lib/HttpClients/Story.client.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@angular/core";
 import {HttpClient} from '@angular/common/http';
-import { IStory, StoryDetail } from "data";
-import { ConfigModule } from "../ConfigModules/WebHttp.module";
+import { IStory} from "data";
 import { WebHttpService } from "../..";
-import { Observable } from "rxjs";
 import { EntityClientService } from "shared/entities";
 //import { EntityClientService } from "./../../../../shared/entities/src/lib/Entities/Entity.client";
 
