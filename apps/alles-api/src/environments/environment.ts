0196f78d66eb38c9107168bce9b5cca50d3afd52
Xin Wang <xx.wang@student.avans.nl>
2022-11-27 15:09:29 +0100

    Modified env variabels

--- apps/alles-api/src/environments/environment.ts
+++ apps/alles-api/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  Database: `mongodb+srv://User33:Das4reich@beifang.mwlxo7r.mongodb.net/?retryWrites=true&w=majority`
+  Database: `mongodb+srv://User33:${process.env["MONGO_CODE"]}@beifang.mwlxo7r.mongodb.net/?retryWrites=true&w=majority`
 };
