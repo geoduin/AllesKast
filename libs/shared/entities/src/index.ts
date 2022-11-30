fb69140591a2b2082762d9afab60be077b557eb7
xin <xin20wang@outlook.com>
2022-11-30 11:13:46 +0100

    Solved merge conflict

--- libs/shared/entities/src/index.ts
+++ libs/shared/entities/src/index.ts
@@ -1,7 +1,3 @@
 export * from './lib/shared-entities.module';
 export * from  './lib/Interfaces/IRepo.interface';
-<<<<<<< HEAD
 export * from './lib/general-entities/entity.clientService';
-=======
-export * from './lib/entity/entity.client';
->>>>>>> 3d1f23fd63e08b9db441fd9cba841deceb3b3b30
