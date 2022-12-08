5523be52aa91daee79ad276d07d0e64bae1f12a9
xin <xin20wang@outlook.com>
2022-12-08 12:11:17 +0100

    Added detail test

--- apps/AllesKast/src/app/features/user/user-detail/user-detail.component.spec.ts
+++ apps/AllesKast/src/app/features/user/user-detail/user-detail.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PrivateUser } from '../../../../../../../libs/data/src';
+import { AuthService, UserClient } from '../../../../../../../libs/services/src';
+import { DummyRepo } from '../../../../../../../libs/services/src/lib/Dummy/DummyRepo';
 
 import { UserDetailComponent } from './user-detail.component';
 
@@ -6,9 +11,43 @@ describe('UserDetailComponent', () => {
   let component: UserDetailComponent;
   let fixture: ComponentFixture<UserDetailComponent>;
 
+  //DummyRepo
+  let dummyRepo: DummyRepo;
+
+  //Mockservice
+  let MockAuthService;
+  let MockRouter;
+  let MockUserClient;
+  let MockActivatedRoute;
+
+  //Mock observables editable$ en DetailUser;
+  let OtherMockUser: PrivateUser
+  let OwnMockUser: PrivateUser
+  let MockEditable$: Observable<boolean>;
+
   beforeEach(async () => {
+
+    //Maak mock waarde;
+    MockUserClient =  jasmine.createSpyObj('UserClient', ['GetAll', 'GetOne', 'subscribe']);
+    MockAuthService = jasmine.createSpyObj('AuthService', ['IsEditable']);
+    MockActivatedRoute = jasmine.createSpyObj('ActivatedRoute', ['subscribe'])
+    MockRouter = jasmine.createSpyObj('Router', ['navigate']);
+
+    //Dummyrepo
+    dummyRepo = new DummyRepo();
+
+    //Wijs mockattributen toe;
+    //OtherMockUser = dummyRepo.UserArray[0];
+    //OwnMockUser = dummyRepo.UserArray[1];
+
     await TestBed.configureTestingModule({
-      declarations: [ UserDetailComponent ]
+      declarations: [ UserDetailComponent ],
+      providers: [
+        {provide: UserClient, useValue: MockUserClient},
+        {provide: AuthService, useValue: MockAuthService},
+        {provide: Router, useValue: MockRouter},
+        {provide: ActivatedRoute, useValue: MockActivatedRoute}
+      ]
     })
     .compileComponents();
 
@@ -20,4 +59,5 @@ describe('UserDetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
